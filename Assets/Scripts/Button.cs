d977f65cf861e19039b3a41ddd10f7f8d417a173
Jakub Šindelář <25355950+megastary@users.noreply.github.com>
2020-11-25 23:05:19 +0100

    First commit, 3 clickable buttons.
    Production ready build settings, switched to new Input system.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private void Start()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void ButtonClick()
+    {
+        text.text += "C";
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using Mouse = UnityEngine.InputSystem.Mouse;
+
+// This may be used later. Currently using basic Input System UI Input Module
+
+public class InputManager : MonoBehaviour, IPointerClickHandler
+{
+    private Mouse mouse = Mouse.current;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+    }
+
+    private void Awake()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void Update()
+    {
+
+        if (Touch.activeFingers.Count == 1)
+        {
+            Touch activeTouch = Touch.activeFingers[0].currentTouch;
+
+            Debug.Log($"Phase: {activeTouch.phase} | Position: {activeTouch.startScreenPosition}");
+        }
+    }
+}
